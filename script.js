68b6d0412d3e54bf50daf59e42c668e84bec050c
Dani Guardiola <dani.guardiola@datatrends.es>
2015-01-24 15:27:52 +0100

    New ver

--- script.js
+++ script.js
@@ -2,3 +2,11 @@ var paperkit = new Materializer();
 window.addEventListener('load', function(){
 	paperkit.init();
 });
+
+function goTo(el){
+	var id = el.getAttribute('data-page');
+	var page = document.getElementById(id);
+	page.scrollIntoViewIfNeeded();
+	document.body.scrollIntoView();
+	paperkit.sidemenu.close();
+}
